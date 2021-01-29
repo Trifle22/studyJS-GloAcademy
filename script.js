5afdf53290e360d0f63c092c3ba2dea19c58a11c
trifle22 <xromchenkov99@gmail.com>
2021-01-29 13:59:27 +0300

    2nd lesson

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-let money, income, addExpenses, deposit, mission, period;
-alert('hello GitHub');
-console.log('hello GitHub');
+let money, income, addExpenses, deposit, mission, period, budgetDay;
 
+
+money = 50000;
+income = 'инвестиции';
+addExpenses = 'интернет, телефон, проезд';
+deposit = true;
+mission = 500000;
+period = 6;
+budgetDay = money / 30;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель -  заработать ' + mission + ' рублей');
+console.log(addExpenses.toLowerCase().split(', '));
+console.log(budgetDay);
