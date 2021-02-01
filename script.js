de956ba7275ab61549b553cc44b0ee6443eff91c
trifle22 <xromchenkov99@gmail.com>
2021-02-01 13:46:39 +0300

    lesson03 homework

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-let money, income, addExpenses, deposit, mission, period, budgetDay;
+'use strict';
+
+let money, income, addExpenses, deposit, mission, period, budgetDay, budgetMonth, purpose,
+    expenses1, expenses2, amount1, amount2;
 
 
-money = 50000;
+money = +prompt('Ваш месячный доход?');
 income = 'инвестиции';
-addExpenses = 'интернет, телефон, проезд';
-deposit = true;
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+deposit = confirm('Есть ли у вас есть депозит в банке ?');
 mission = 500000;
 period = 6;
-budgetDay = money / 30;
+expenses1 = prompt('Введите обязательную статью расходов');
+amount1 = +prompt('Во сколько это обойдется?');
+expenses2 = prompt('Введите обязательную статью расходов');
+amount2 = +prompt('Во сколько это обойдется?');
+
+budgetMonth = money - (amount1 + amount2);
+budgetDay = Math.floor(budgetMonth/30);
+purpose = Math.ceil(mission/budgetMonth);
 
 console.log(typeof money);
 console.log(typeof income);
@@ -15,5 +25,18 @@ console.log(typeof deposit);
 console.log(addExpenses.length);
 console.log('Период равен ' + period + ' месяцев');
 console.log('Цель -  заработать ' + mission + ' рублей');
-console.log(addExpenses.toLowerCase().split(', '));
-console.log(budgetDay);
+console.log(addExpenses);
+console.log(addExpenses.toLowerCase().split(','));
+console.log('Месячный бюджет равен ' + budgetMonth + 'р.');
+console.log('Цель будет достигнута за ' + purpose + ' месяцев(-a)');
+console.log('Бюджет на день равен: ' + budgetDay + 'p.');
+
+if (budgetDay >= 1200) {
+  console.log('У вас высокий уровень дохода');
+} else if (budgetDay >= 600 && budgetDay < 1200) {
+  console.log('У вас средний уровень дохода');
+} else if (budgetDay < 600 && budgetDay >= 0) {
+  console.log('К сожалению, у вас уровень дохода ниже среднего');
+} else if (budgetDay < 0) {
+  console.log('Что-то пошло не так');
+}
