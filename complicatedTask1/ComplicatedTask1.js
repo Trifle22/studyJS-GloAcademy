e6028b13f24a615743e5648859f05bc51ae9fa36
Trifle22 <xromchenkov99@gmail.com>
2021-02-23 02:01:06 +0300

    complicatedTask1

--- complicatedTask1/ComplicatedTask1.js
+++ complicatedTask1/ComplicatedTask1.js
@@ -1,1 +1,23 @@
-console.log(1);
+const text = document.querySelector('.main-text');
+const input = document.querySelector('.main-input');
+
+function writeText() {
+  text.textContent = input.value;
+}
+
+const debounceFunc = func => {
+  let timeout;
+  return function() {
+    const fnCall = () => {
+      func.apply(this);
+    };
+    clearTimeout(timeout);
+    timeout = setTimeout(fnCall, 300);
+  };
+};
+
+const debounceWriteText = debounceFunc(writeText);
+
+
+input.addEventListener('input', debounceWriteText);
+
