f009d14d117af835c4b2bb49d87bacc3198a6f13
Trifle22 <xromchenkov99@gmail.com>
2021-02-23 22:54:52 +0300

    +scroll smooth

--- script/script.js
+++ script/script.js
@@ -110,8 +110,24 @@ window.addEventListener('DOMContentLoaded', () => {
 
     popupClose.addEventListener('click', () => {
       popup.style.display = 'none';
-
     });
+
+    function scrollToAnchors() {
+      const anchors = document.querySelectorAll('a[href*="#"]');
+      anchors.forEach(item => {
+        item.addEventListener('click', event => {
+          event.preventDefault();
+          const anchorID = item.getAttribute('href').substr(1);
+
+          document.getElementById(anchorID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start',
+          });
+        });
+      });
+    }
+
+    scrollToAnchors();
   };
 
 
