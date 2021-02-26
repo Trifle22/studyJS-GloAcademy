b9b5c5540e5833d5429358f59fb914e447140bda
Trifle22 <xromchenkov99@gmail.com>
2021-02-27 02:06:22 +0300

    +complete tasks

--- lesson22OOP-ToDo/js/script.js
+++ lesson22OOP-ToDo/js/script.js
@@ -1,77 +1,107 @@
 /* eslint-disable linebreak-style */
 class Todo {
-    constructor(form, input, todoList, todoCompleted) {
-        this.form = document.querySelector(form);
-        this.input = document.querySelector(input);
-        this.todoList = document.querySelector(todoList);
-        this.todoCompleted = document.querySelector(todoCompleted);
-        this.todoData = new Map(JSON.parse(localStorage.getItem('toDoList')));
-    }
+  constructor(form, input, todoList, todoCompleted) {
+    this.form = document.querySelector(form);
+    this.input = document.querySelector(input);
+    this.todoList = document.querySelector(todoList);
+    this.todoCompleted = document.querySelector(todoCompleted);
+    this.todoData = new Map(JSON.parse(localStorage.getItem('toDoList')));
+  }
 
-    addToStorage() {
-        localStorage.setItem('toDoList', JSON.stringify([...this.todoData]));
-    }
+  addToStorage() {
+    localStorage.setItem('toDoList', JSON.stringify([...this.todoData]));
+  }
 
-    render() {
-        this.todoList.textContent = '';
-        this.todoCompleted.textContent = '';
-        this.todoData.forEach(this.createItem, this);
-        this.addToStorage();
-    }
+  render() {
+    this.todoList.textContent = '';
+    this.todoCompleted.textContent = '';
+    this.todoData.forEach(this.createItem, this);
+    this.addToStorage();
+  }
 
-    createItem(todo) {
-        const li = document.createElement('li');
-        li.classList.add('todo-item');
-        li.key = todo.key;
-        li.insertAdjacentHTML('beforeend', `
+  createItem(todo) {
+    const li = document.createElement('li');
+    li.classList.add('todo-item');
+    li.key = todo.key;
+    li.insertAdjacentHTML('beforeend', `
         <span class="text-todo">${todo.value}</span>
         <div class="todo-buttons">
         <button class="todo-remove"></button>
         <button class="todo-complete"></button>
         </div>`
-        );
+    );
 
-        if (todo.todoCompleted) {
-            this.todoCompleted.append(li);
-        } else {
-            this.todoList.append(li);
-        }
+    if (todo.todoCompleted) {
+      this.todoCompleted.append(li);
+    } else {
+      this.todoList.append(li);
     }
+  }
 
-    addTodo(e) {
-        e.preventDefault();
-        if (this.input.value.trim()) {
-            const newTodo = {
-                value: this.input.value,
-                todoCompleted: false,
-                key: this.generateKey(),
-            };
-            this.todoData.set(newTodo.key, newTodo);
+  addTodo(e) {
+    e.preventDefault();
+    if (this.input.value.trim()) {
+      const newTodo = {
+        value: this.input.value,
+        todoCompleted: false,
+        key: this.generateKey(),
+      };
+      this.todoData.set(newTodo.key, newTodo);
+      this.render();
+    } else {
+      alert('Пустое поле добавить нельзя');
+    }
+  }
+
+  generateKey() {
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  }
+
+  deleteItem() {
+
+  }
+
+  completeItem() {
+
+  }
+
+  handler() {
+    this.todoList.addEventListener('click', event => {
+      const target = event.target;
+      if (target.closest('button.todo-complete')) {
+        const items = JSON.parse(localStorage.getItem('toDoList'));
+        const todoItem = target.closest('.todo-item');
+        items.forEach(item => {
+          if (item[0] === todoItem.key) {
+            item[1].todoCompleted = true;
+            this.todoData = new Map(items);
             this.render();
-        }
-    }
+          }
+        });
+      }
+    });
+    this.todoCompleted.addEventListener('click', event => {
+      const target = event.target;
+      if (target.closest('button.todo-complete')) {
+        const items = JSON.parse(localStorage.getItem('toDoList'));
+        const todoItem = target.closest('.todo-item');
+        items.forEach(item => {
+          if (item[0] === todoItem.key) {
+            item[1].todoCompleted = false;
+            this.todoData = new Map(items);
+            this.render();
+          }
+        });
+      }
+    });
+  }
 
-    generateKey() {
-        return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-    }
-
-    deleteItem() {
-
-    }
-
-    completeItem() {
-
-    }
-
-    handler() {
-        //делегирование
-    }
-
-    init() {
-        this.form.addEventListener('submit', this.addTodo.bind(this));
-        this.render();
-    }
+  init() {
+    this.form.addEventListener('submit', this.addTodo.bind(this));
+    this.render();
+  }
 }
 const todo = new Todo('.todo-control', '.header-input', '.todo-list', '.todo-completed');
 
 todo.init();
+todo.handler();
