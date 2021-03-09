7748be553729071e59d0183e30155af3e8e9bb24
Trifle22 <xromchenkov99@gmail.com>
2021-03-10 00:34:25 +0300

    commitbefore homework

--- ajax_car/script.js
+++ ajax_car/script.js
@@ -1,11 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-    'use strict';
-
     const select = document.getElementById('cars'),
         output = document.getElementById('output');
-
     select.addEventListener('change', () => {
 
-
+      fetch('./cars.json')
+      .then((response) => {
+        if (response.status !== 200) {
+          throw new Error('status netwwork not 200.');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        data.cars.forEach(item => {
+          if (item.brand === select.value) {
+            const {brand, model, price} = item;
+            output.innerHTML = `Тачка ${brand} ${model} <br>
+            Цена: ${price}`;
+          }
+        })
+      })
+      .catch((error) => console.error(error));
     });
 })
