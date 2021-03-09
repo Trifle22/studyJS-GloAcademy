5ca46c015895a2c0f23aae2e4dba252a714f5550
Trifle22 <xromchenkov99@gmail.com>
2021-03-09 23:57:41 +0300

    lesson29 first commit from xmlhttp... to fetch

--- ajax_car/script.js
+++ ajax_car/script.js
@@ -5,41 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
         output = document.getElementById('output');
 
     select.addEventListener('change', () => {
-      const getCar = () => {
-        return new Promise((resolve, reject) => {
-          const request = new XMLHttpRequest();
-          request.open('GET', './cars.json');
-          request.setRequestHeader('Content-type', 'application/json');
-          request.send();
-          request.addEventListener('readystatechange', () => {
-              if (request.status === 200) {
-                if (request.responseText) {
-                  const data = JSON.parse(request.responseText);
-                  console.log(data);
-                  resolve(data);
-                }
-              } else {
-                reject(Error);
-              }
-          });
-        })
-      }
 
-      getCar()
-      .then((data) => {
-        data.cars.forEach(item => {
-          if (item.brand === select.value) {
-              const {brand, model, price} = item;
-              output.innerHTML = `Тачка ${brand} ${model} <br>
-              Цена: ${price}$`;
-          }
-      });
-      })
-      .catch((error) => {
-        console.log(error);
-        output.innerHTML = error;
-      })
 
     });
-    
-});
+})
