32f34c0350248fad418658f2c2666cae0a6af788
Trifle22 <xromchenkov99@gmail.com>
2021-03-09 20:51:17 +0300

    +promise

--- ajax_car/script.js
+++ ajax_car/script.js
@@ -5,24 +5,40 @@ document.addEventListener('DOMContentLoaded', () => {
         output = document.getElementById('output');
 
     select.addEventListener('change', () => {
-        const request = new XMLHttpRequest();
-        request.open('GET', './cars.json');
-        request.setRequestHeader('Content-type', 'application/json');
-        request.send();
-        request.addEventListener('readystatechange', () => {
-            if (request.readyState === 4 && request.status === 200) {
-                const data = JSON.parse(request.responseText);
-                data.cars.forEach(item => {
-                    if (item.brand === select.value) {
-                        const {brand, model, price} = item;
-                        output.innerHTML = `Тачка ${brand} ${model} <br>
-                        Цена: ${price}$`;
-                    }
-                });
-            } else {
-                output.innerHTML = 'Произошла ошибка';
-            }
-        });
+      const getCar = () => {
+        return new Promise((resolve, reject) => {
+          const request = new XMLHttpRequest();
+          request.open('GET', './cars.json');
+          request.setRequestHeader('Content-type', 'application/json');
+          request.send();
+          request.addEventListener('readystatechange', () => {
+              if (request.status === 200) {
+                if (request.responseText) {
+                  const data = JSON.parse(request.responseText);
+                  console.log(data);
+                  resolve(data);
+                }
+              } else {
+                reject(error);
+              }
+          });
+        })
+      }
+
+      getCar()
+      .then((data) => {
+        data.cars.forEach(item => {
+          if (item.brand === select.value) {
+              const {brand, model, price} = item;
+              output.innerHTML = `Тачка ${brand} ${model} <br>
+              Цена: ${price}$`;
+          }
+      });
+      })
+      .catch((error) => {
+        output.innerHTML = error;
+      })
+
     });
 
 });
